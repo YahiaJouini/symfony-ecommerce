4e42c5c5eba5735edc3ebc691a4798d138d0ee28
YahiaJouini <jouiniyahya117@gmail.com>
2025-04-22 19:54:52 +0100

    feat: Integrate KnpPaginatorBundle for pagination; update product management templates and enhance layout

--- src/Controller/DashboardProductController.php
+++ src/Controller/DashboardProductController.php
@@ -3,27 +3,39 @@
 namespace App\Controller;
 
 use App\Entity\Product;
-use App\Entity\Category;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 #[Route('/dashboard/product')]
 final class DashboardProductController extends AbstractController
 {
     #[Route('/', name: 'admin_product')]
-    public function index(ProductRepository $productRepository): Response
-    {
-        $products = $productRepository->findAll();
-        
+    public function index(
+        ProductRepository $productRepository,
+        Request $request,
+        PaginatorInterface $paginator
+    ): Response {
+        $query = $productRepository->createQueryBuilder('p')
+        ->orderBy('p.id', 'ASC')
+        ->getQuery();
+    
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
+    
         return $this->render('dashboard/product/index.html.twig', [
-            'products' => $products,
+            'pagination' => $pagination,
         ]);
     }
 
