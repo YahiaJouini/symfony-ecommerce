f4ae92a90be2e0a1e302db4cd1ec600035c5bb00
YahiaJouini <jouiniyahya117@gmail.com>
2025-04-22 19:58:24 +0100

    feat: Implement pagination for product listing; update ProductController and templates to use paginated data

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,20 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use Knp\Component\Pager\PaginatorInterface;
 
 final class ProductController extends AbstractController
 {
     #[Route('/product', name: 'app_product')]
-    public function index(ProductRepository $repository): Response
+    public function index(
+        ProductRepository $repository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response
     {
-
-        $products = $repository->findAll();
+        $query = $repository->createQueryBuilder('p')->getQuery();
+        
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            6
+        );
         
         return $this->render('product/index.html.twig', [
-            'products' => $products,
+            'pagination' => $pagination,
         ]);
     }
 }
